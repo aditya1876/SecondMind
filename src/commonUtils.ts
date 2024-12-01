6cdfa067abb14fb1f25743779ff7c7c793bf6b60
adi <adikakaam@gmail.com>
2024-12-01 11:49:33 +0530

    Added signin route, util functions for password hash and compare, jwt token generation

--- src/commonUtils.ts
+++ src/commonUtils.ts
@@ -1,17 +1,9 @@
 import zod from "zod";
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
 
 //ZOD SCHEMAS============
-// const usernameSchema = zod
-//   .string()
-//   .min(3, { message: "username should be atleast 3 characters" })
-//   .max(10, { message: "Username should be less than 10 characters." });
-// const passwordSchema = zod
-//   .string()
-//   .min(8, { message: "password should be atleast 8 characters" })
-//   .max(20, { message: "password should be less than 21 characters" });
-
 const userSchema = zod.object({
   username: zod
     .string()
@@ -50,19 +42,58 @@ export function parseCreds(
     });
     return;
   }
-  // const usernameParse = usernameSchema.safeParse(req.body.username);
-  // const passwordParse = passwordSchema.safeParse(req.body.password);
-  // if (!usernameParse.success || !passwordParse.success) {
-  //   res.status(411).json({
-  //     message: "Username or password are not in valid format",
-  //   });
-  //   return;
-  // }
 
   next();
 }
 
 //=======================
 //OTHER FUNCTIONS========
-//
+//Hash passowrd of the user before saving in db
+export async function hashPassword(
+  password: string,
+): Promise<string | undefined> {
+  console.log(`Hashing password`);
+  try {
+    const hashedPassword = await bcrypt.hash(password, 5);
+    console.log("Password Hashed");
+    console.log(`Hashed pass: [${hashedPassword}]`);
+    return hashedPassword;
+  } catch (e) {
+    console.log("Error during hashing password");
+    console.log(e);
+    return;
+  }
+}
+
+//Compare userpassword with hashed db password
+export async function comparePasswords(
+  userPassword: string,
+  dbPassword: string,
+): Promise<boolean | undefined> {
+  console.log("Verifying password provided by user...");
+  try {
+    const passwordCompare = await bcrypt.compare(userPassword, dbPassword);
+    console.log(`Vefication completed. Result: [${passwordCompare}]`);
+    return passwordCompare;
+  } catch (e) {
+    console.log("Error during password comparision");
+    console.log(e);
+    return;
+  }
+}
+
+//generate JWT token for user that is signing in
+export function generateUserToken(username: string): string | undefined {
+  console.log(`Generating jwt token for the user: [${username}]`);
+  try {
+    const jwt_secret = process.env.JWT_SECRET!;
+    const token = jwt.sign({ username: username }, jwt_secret);
+    console.log(`Token generated for the user: [${username}] succesfully!`);
+    return token;
+  } catch (e) {
+    console.log(`Error generating token for user: [${username}]`);
+    console.log(e);
+    return; //handle undefined error when reading token value in calling funciton
+  }
+}
 //=======================
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { Request, Response } from "express";
-import { parseCreds } from "./commonUtils";
+import {
+  parseCreds,
+  generateUserToken,
+  hashPassword,
+  comparePasswords,
+} from "./commonUtils";
 import { User } from "./dataTypes";
 import { connectToDB, UsersCollection } from "./db";
+import { string } from "zod";
 
 const app = express();
 app.use(express.json());
@@ -27,11 +33,22 @@ app.post(
       username: req.body.username,
       password: req.body.password,
     };
+
+    const hashedPassword: string | undefined = await hashPassword(
+      user.password,
+    ); //hash the password
+    if (typeof hashedPassword === "undefined") {
+      console.log(`Error occured during password hash operation`);
+      res.status(500).json({
+        message: "Something went wrong, please try again!",
+      });
+    }
+
     try {
       //check data exists in database
       const userExists = await UsersCollection.create({
         username: user.username,
-        password: user.password,
+        password: hashedPassword,
       });
 
       userExists
@@ -51,6 +68,63 @@ app.post(
       });
     }
 
+    console.log("<--");
+  },
+);
+
+app.post(
+  "/api/v1/signin",
+  parseCreds,
+  async function (req: Request, res: Response) {
+    console.log("Signin flow -->");
+    const user: User = {
+      username: req.body.username,
+      password: req.body.password,
+    };
+
+    //check if user already exists
+    const userExists = await UsersCollection.findOne({
+      username: user.username,
+    });
+    console.log(`UserExists: [${userExists}]`);
+    if (userExists == null) {
+      res.status(403).json({
+        message: `User with username: [${user.username}] not found!`,
+      });
+    } else {
+      //compare passord
+      const comparePassword = await comparePasswords(
+        user.password,
+        userExists.password,
+      );
+
+      if (typeof comparePassword === "undefined") {
+        console.log(`Error occured during password compare operation`);
+        res.status(500).json({
+          message: "Something went wrong, please try again!",
+        });
+      }
+      if (comparePassword) {
+        //generate token for the user
+        const token: String | undefined = generateUserToken(user.username); //generate token
+        if (typeof token === "undefined") {
+          console.log(`Token genaration failed for user [${user.username}]`);
+          res.status(500).json({
+            message: "Unable to generate token. Please try again!",
+          });
+        } else if (typeof token === "string") {
+          console.log(`Signin successful. token: [${token}]`);
+          res.status(200).json({
+            token: token,
+          });
+        }
+      } else {
+        console.log(`Password comparision failed for user [${user.username}]`);
+        res.status(403).json({
+          message: "incorrect username and passoword combination",
+        });
+      }
+    }
     console.log("<--");
   },
 );
